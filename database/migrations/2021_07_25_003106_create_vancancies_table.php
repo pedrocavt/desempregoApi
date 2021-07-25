cad65dd8f3adb3e1fe786b2356f5a712f195d1d5
Pedro Vitor <pedrocavt@gmail.com>
2021-07-24 23:07:16 -0300

    first commit

---
+++ database/migrations/2021_07_25_003106_create_vancancies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVancanciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vancancies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 60);
+            $table->text('description');
+            $table->integer('wage');
+            $table->timestamps();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vancancies');
+    }
+}
